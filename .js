92b5fde607ae7bce0d8a78224ddbac10c81b04b6
Georgiy666 <101511357+Georg1y666@users.noreply.github.com>
2022-03-13 20:44:01 +0600

    Test
    Testir

---
+++ .js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: "Solana Docs",
+  tagline:
+    "Solana is an open source project implementing a new, high-performance, permissionless blockchain.",
+  url: "https://docs.solana.com",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
